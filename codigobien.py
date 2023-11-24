ddc81c0b9721aac3955c0529a0d0137122cad407
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-24 17:08:25 +0100

    mejor

--- codigobien.py
+++ codigobien.py
@@ -5,16 +5,40 @@ class Calculadora:
         self.num2 = num2
 
     def sumar (self):
-        return self.num1 + self.num2
+        try: 
+            self.num1 = int(self.num1)
+            self.num2 = int(self.num2)
+            suma = self.num1 + self.num2
+            return suma
+        except ValueError:
+            print("No se puede sumar letras")
     def restar (self):
-        return self.num1 - self.num2
+        try:
+            self.num1 = int(self.num1)
+            self.num2 = int(self.num2)
+            restar = self.num1 - self.num2
+            return restar
+        except ValueError:
+            print("No se puede restar letras")
+        
     def multiplicar (self):
-        return self.num1 * self.num2
+        try: 
+            self.num1 = int(self.num1)
+            self.num2 = int(self.num2)
+            multiplicar = self.num1 * self.num2
+            return multiplicar
+        except ValueError:
+            print("No se puede multiplicar letras")
     def dividir (self):
-        if self.num2 != 0:
-            return self.num1 / self.num2
-        else:
-            print("No se puede dividir entre cero.")
-
+        try:
+            self.num1 = int(self.num1)
+            self.num2 = int(self.num2)
+            if self.num2 != 0:
+                dividir = self.num1 / self.num2
+                return dividir
+            else:
+                print("No se puede dividir entre cero.")
+        except ValueError:
+            print("No se puede dividir letras")
 
     
