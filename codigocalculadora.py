f7152c532d4016950d355cb957bf8905703ef4ee
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-24 18:26:29 +0100

    comentarios

--- codigocalculadora.py
+++ codigocalculadora.py
@@ -1,39 +1,51 @@
 
 class Calculadora:
+    '''
+    Clase Calculadora encargada de revisar los datos ingresados por el usuario y 
+    realizar las operaciones correspondientes.
+    '''
     def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
 
     def sumar (self):
+        #Miramos si los datos ingresados son números y si no, devolvemos un mensaje de error
         try: 
             self.num1 = float(self.num1)
             self.num2 = float(self.num2)
+            #realizamos la operación
             suma = self.num1 + self.num2
             return suma
         except ValueError:
             return("No se puede sumar letras")
     def restar (self):
+        #Miramos si los datos ingresados son números y si no, devolvemos un mensaje de error
         try:
             self.num1 = float(self.num1)
             self.num2 = float(self.num2)
+            #realizamos la operación
             restar = self.num1 - self.num2
             return restar
         except ValueError:
             return("No se puede restar letras")
         
     def multiplicar (self):
+        #Miramos si los datos ingresados son números y si no, devolvemos un mensaje de error
         try: 
             self.num1 = float(self.num1)
             self.num2 = float(self.num2)
+            #realizamos la operación
             multiplicar = self.num1 * self.num2
             return multiplicar
         except ValueError:
             return("No se puede multiplicar letras")
         
     def dividir (self):
+        #Miramos si los datos ingresados son números y si no, devolvemos un mensaje de error
         try:
             self.num1 = float(self.num1)
             self.num2 = float(self.num2)
+            #miramos si el divisor es cero y si no, realizamos la operación
             if self.num2 != 0:
                 dividir = self.num1 / self.num2
                 return dividir
--- ruta.py
+++ ruta.py
@@ -5,22 +5,27 @@ import secrets
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)  
 
+#Ruta de la página principal
 @app.route('/home')
 def index():
     return render_template('pagCalculadora.html')
 
-
+#Ruta calcular
 @app.route('/calculadora', methods=['GET', 'POST'])
 def calculadora():
     if request.method == 'POST':
+        #recogemos los datos del formulario
         num1 = request.form.get('num1')
         num2 = request.form.get('num2')
         operador = request.form.get('operador')
 
+        #creamos el objeto calculadora
         calculadora = Calculadora(num1, num2)
+        
+        #identificamos el operador y llamamos a la función correspondiente
         if operador == 'sumar':
             resultado = calculadora.sumar()
- 
+            
         elif operador == 'restar':
             resultado = calculadora.restar()
 
@@ -30,6 +35,7 @@ def calculadora():
         elif operador == 'dividir':
             resultado = calculadora.dividir()
         
+        #mensaje para mostrar el resultado
         mensaje = 'El resultado es: ' + str(resultado)
         flash(mensaje, 'success')
     return render_template('mensaje_resultados.html', mensaje=mensaje)
